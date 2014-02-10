739eb1b029517b884bae2bfaed37945acc5a66de
Fredrik <fredrik@ubuntu.(none)>
2014-02-10 11:43:05 -0800

    Change form add dummy messages

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,20 +1,34 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
+(defn home [& [name message error]]
   (layout/common
    [:h1 "Guestbook"]
    [:p "Welcome to my guestbook"]
+   [:p error]
+   ;;call show-guests to display comments
+   (show-guests)
    [:hr]
-   [:form
+   (form-to [:post "/"]
     [:p "Name:"]
-    [:input]
+    (text-field "name" name)
     [:p "Message:"]
-    [:textarea {:rows 10 :cols 40}]]))
+    (text-area {:rows 10 :cols 40} "message" message)
+    [:br]
+    (submit-button "comment"))))
+
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bobby" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]])])
 
 (defroutes home-routes
   (GET "/" [] (home)))
 
-(defroutes same-route
-  (GET "/fred" [] (home)))
