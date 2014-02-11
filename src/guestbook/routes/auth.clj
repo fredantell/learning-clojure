70654aff7a3c1bd891a676e50dd3c45a7de342d1
Fred Antell <fred@fredantell.com>
2014-02-11 11:25:53 -0800

    Add login page.  Not finished with route handler and need sessesion mgmt

--- src/guestbook/routes/auth.clj
+++ src/guestbook/routes/auth.clj
@@ -3,7 +3,8 @@
             [guestbook.views.layout :as layout]
             [hiccup.form :refer
              [form-to label text-field password-field submit-button]]
-            [noir.response :refer [redirect]]))
+            [noir.response :refer [redirect]]
+            [noir.session :as session]))
 
 (defn control [field name text]
   (list (label name text)
@@ -17,10 +18,25 @@
             (control password-field :pass "Password")
             (control password-field :pass1 "Retype Password")
             (submit-button "Create Account"))))
+(defn login-page []
+  (layout/common
+   (form-to [:post "/login"]
+            (control text-field :id "Screen name")
+            (control password-field :pass "Password")
+            (submit-button "Login"))))
+
 
 (defroutes auth-routes
   (GET "/register" [_] (registration-page))
   (POST "/register" [id pass pass1]
         (if (= pass pass1)
           (redirect "/")
-          (registration-page))))
+          (registration-page)))
+  (GET "/login" [_] (login-page))
+  (POST "/login" [id pass]
+        (session/put! :user id)
+        (redirect "/")))
+
+;; (do
+;;           (println (str "\nlogged in as: " id "\nwith pwd: " pass))
+;;           (redirect "/"))
