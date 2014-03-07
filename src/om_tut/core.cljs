34ee32168511dfa072e051e4996b0ae96ea92bdb
Fredrik Antell <fred@fredantell.com>
2014-03-07 13:56:56 -0500

    Animal list rendering

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -6,52 +6,23 @@
 
 (enable-console-print!)
 
-(def app-state (atom
-                {:contacts
-                 [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-                  {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-                  {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-                  {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-                  {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-                  {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+;; (def app-state (atom
+;;                 {:contacts
+;;                  [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+;;                   {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+;;                   {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+;;                   {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+;;                   {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+;;                   {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(defn middle-name [{:keys [middle middle-initial] :as contact}]
-  (cond
-   middle (str " " middle)
-   middle-initial (str " " middle-initial ".")))
+(def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
 
-(defn display-name [{:keys [first last email] :as contact}]
-  (str last ", " first (middle-name contact)))
+(defn list-view [app owner]
+  (apply dom/ul nil
+         (map #(dom/li nil %) (:list app))))
 
-
-(defn contact-view [contact owner]
-  (reify
-    om/IRenderState
-    (render-state [this {:keys [delete]}]
-            (dom/li nil
-              (dom/span nil (display-name contact))
-              (dom/button #js {:onClick (fn [e] (put! delete contact))} "Delete")))))
-
-(defn contacts-view [app owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-                {:delete (chan)})
-    om/IWillMount
-    (will-mount [_]
-                (let [delete (om/get-state owner :delete)]
-                  (go (while true
-                        (let [contact (<! delete)]
-                          (om/transact! app :contacts
-                            (fn [])))))))
-    om/IRenderState
-    (render-state [this app]
-      (dom/div nil
-               (dom/ul nil
-                       (contact-view app owner))))))
-
-(om/root contacts-view app-state
-         {:target (. js/document (getElementById "contacts"))})
+(om/root list-view app-state
+         {:target (. js/document (getElementById "app"))})
 
 
 
@@ -59,18 +30,91 @@
 
 
 
-#_(defn contact-view [contact owner]
-  (reify
-    om/IRender
-    (render [this]
-            (dom/li nil
-              (dom/span nil (display-name contact))
-              (dom/button nil "Delete")))))
 
-#_(defn contacts-view [app owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/div nil?(dom/h2 nil "Contact List")
-               (apply dom/ul nil
-                 (om/build-all contact-view (:contacts app)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; (defn middle-name [{:keys [middle middle-initial] :as contact}]
+;;   (cond
+;;    middle (str " " middle)
+;;    middle-initial (str " " middle-initial ".")))
+
+;; (defn display-name [{:keys [first last email] :as contact}]
+;;   (str last ", " first (middle-name contact)))
+
+
+;; (defn contact-view [contact owner]
+;;   (reify
+;;     om/IRenderState
+;;     (render-state [this {:keys [delete]}]
+;;             (dom/li nil
+;;               (dom/span nil (display-name contact))
+;;               (dom/button #js {:onClick (fn [e] (put! delete contact))} "Delete")))))
+
+;; (defn contacts-view [app owner]
+;;   (reify
+;;     om/IInitState
+;;     (init-state [_]
+;;                 {:delete (chan)})
+;;     om/IWillMount
+;;     (will-mount [_]
+;;                 (let [delete (om/get-state owner :delete)]
+;;                   (go (while true
+;;                         (let [contact (<! delete)]
+;;                           (om/transact! app :contacts
+;;                             (fn [])))))))
+;;     om/IRenderState
+;;     (render-state [this app]
+;;       (dom/div nil
+;;                (dom/ul nil
+;;                        (contact-view app owner))))))
+
+;; (om/root contacts-view app-state
+;;          {:target (. js/document (getElementById "contacts"))})
+
+
+
+
+
+
+
+;; #_(defn contact-view [contact owner]
+;;   (reify
+;;     om/IRender
+;;     (render [this]
+;;             (dom/li nil
+;;               (dom/span nil (display-name contact))
+;;               (dom/button nil "Delete")))))
+
+;; #_(defn contacts-view [app owner]
+;;   (reify
+;;     om/IRender
+;;     (render [this]
+;;       (dom/div nil?(dom/h2 nil "Contact List")
+;;                (apply dom/ul nil
+;;                  (om/build-all contact-view (:contacts app)))))))
